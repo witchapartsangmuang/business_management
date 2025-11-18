c2d38bb7b6e1dfab5ad17ca18746aaf5b7493c98
witchapart.s <witchapart.s@gmail.com>
2025-11-18 15:20:44 +0700

    add select component

--- frontend/src/components/test.tsx
+++ frontend/src/components/test.tsx
@@ -16,6 +16,16 @@ export default function TestComponent() {
                 <option>Option 1</option>
                 <option>Option 2</option>
             </select>
+            <select
+                className="block w-72 rounded-md border border-slate-300 bg-white px-3 py-2 text-slate-700 shadow-sm text-sm
+         focus:border-blue-500 focus:ring-2 focus:ring-blue-500/25 focus:outline-none
+         appearance-none bg-no-repeat bg-[right_0.75rem_center] bg-[length:14px]
+         bg-[url(data:image/svg+xml,%3Csvg%20xmlns%3D%27http%3A//www.w3.org/2000/svg%27%20viewBox%3D%270%200%2016%2016%27%3E%3Cpath%20fill%3D%27none%27%20stroke%3D%27%23343a40%27%20stroke-linecap%3D%27round%27%20stroke-linejoin%3D%27round%27%20stroke-width%3D%272%27%20d%3D%27M2%205l6%206%206-6%27/%3E%3C/svg%3E)]"
+            >
+                <option selected>Select an option</option>
+                <option>Option 1</option>
+                <option>Option 2</option>
+            </select>
         </>
     )
 }
